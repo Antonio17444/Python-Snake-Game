5a9bd23762ddc1ce0642c5ffef93451a5ed60818
Antonio Henrique <antoniohenrique1305l@gmail.com>
2025-04-20 15:31:37 -0300

    Iniciando a base do pygame
    Criação da Janela flutuante (Larg = 800 / Alt = 400) e aplicando um loop verdadeiro até que a condição *Fechar Janela* (Linhas: 18 - 22) seja satisfeita.

--- introducao_SnakeGame.py
+++ introducao_SnakeGame.py
@@ -1,2 +1,29 @@
 import pygame
+import sys 
+
+pygame.init() # iniciando o Pygame - Henrique
+
+# Tamanho da Janela:
+
+largura = 800
+altura = 400
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Snake Game - Henrique and Fernando")
+
+# Contagem de FPS:
+relogio = pygame.time.Clock()
+
+# Loop do Game:
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Inicio da criação do Game:
+
+
+
+
 
