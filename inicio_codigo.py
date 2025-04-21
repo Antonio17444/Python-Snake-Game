96d3b1bb0e4c31be8e5fe97f0a6020ddbe334a0f
Nyh0n <fp354575@gmail.com>
2025-04-21 17:29:49 -0300

    Update inicio_codigo.py
     -- Adições --
    
    + movimentação iqual o jogo original
    + corpo da cobra aumentado com base nos pontos
    
    -- Bugs --
    
    - corpo da cobra esta ficando com a textura da cabeça
     | para ver o jogo sem a textura ir na parte da funcao movercorpo retira # da primeira linha dentro do for e colocar na segunda linha.

--- inicio_codigo.py
+++ inicio_codigo.py
@@ -6,7 +6,7 @@ from random import randint
 largura = 640
 altura = 480
 
-vel = 15
+vel = 7
 
 # Flag para definir se o jogo continua rodando
 
@@ -20,7 +20,10 @@ y = altura/2 # variavel que controla o movimento no eixo y (retangulo azul)
 x_ponto = randint(40,600)
 y_ponto = randint(40,440)
 
+lista_tudo = []
 
+x_controle = vel
+y_controle = 0
 
 # Inicializando modulos do pygame
 
@@ -47,6 +50,14 @@ relogio = pygame.time.Clock()
 contador_de_pontos = 0
 fonte = pygame.font.SysFont('Arial',40,False,True)
 
+# função que faz os pontos irem para o corpo do jogador
+
+def movecorpo(lista_tudo):
+    
+    for i in range (len(lista_tudo)-1,-1,-1):
+        #pygame.draw.rect(tela, (0, 0, 255), (lista_tudo[i][0], lista_tudo[i][1], 40, 40))  # Desenha o retângulo azul atraz do jogador
+        tela.blit(cabeca_img, (lista_tudo[i][0], lista_tudo[i][1])) #esta dando erro na cabeça do boneco
+
 # Loop onde o jogo deve rodar
 
 while rodando:
@@ -54,8 +65,6 @@ while rodando:
     mensagem = f'Pontos: {contador_de_pontos}' # mensagem dentro do while para atualizar os pontos
     texto = fonte.render(mensagem,True,(255,255,255)) #formatando texto
     
-
-
     relogio.tick(60) # fps
 
     tela.fill((0, 0, 0))  # Limpa a tela com cor preta
@@ -63,6 +72,39 @@ while rodando:
     for event in pygame.event.get():  # Detecta eventos
         if event.type == pygame.QUIT:
             rodando = False
+        
+        # ver quais teclas sao presionadas
+        
+        teclas = pygame.key.get_pressed() 
+
+        if teclas[pygame.K_UP]:
+            if y_controle == vel:
+                pass
+            else:
+                y_controle = -vel
+                x_controle = 0
+
+        if teclas[pygame.K_DOWN]:
+            if y_controle == -vel:
+                pass
+            else:
+                y_controle = +vel
+                x_controle = 0
+
+        if teclas[pygame.K_LEFT]:
+            if x_controle == vel:
+                pass
+            else:
+                x_controle = -vel
+                y_controle = 0
+
+        if teclas[pygame.K_RIGHT]:
+            if x_controle == -vel:
+                pass
+            else:    
+                x_controle = vel
+                y_controle = 0
+
 
     # Verifica se o jogador saiu da tela(se sim entao o progama fecha)
 
@@ -75,19 +117,11 @@ while rodando:
     if x <= 0:
         rodando = False
 
-    # ver quais teclas sao presionadas
+    # usando para o jogador nao se mover nas diagonais
 
-    teclas = pygame.key.get_pressed() 
+    x += x_controle
+    y += y_controle
 
-    if teclas[pygame.K_UP]:
-        y -= vel
-    if teclas[pygame.K_DOWN]:
-        y += vel
-    if teclas[pygame.K_LEFT]:
-        x -= vel
-    if teclas[pygame.K_RIGHT]:
-        x += vel
-    
     jogador = tela.blit(cabeca_img, (x, y))
 
     ponto = pygame.draw.rect(tela, (255, 255, 255), (x_ponto, y_ponto, 40, 40))  # Desenha os pontos
@@ -101,6 +135,26 @@ while rodando:
 
     tela.blit(texto,(0,0)) #imprimindo na tela a mensagem
 
+
+    # selecionando o x e y que o jogador passou
+ 
+    lista_cabeca = []
+    lista_cabeca.append(x)
+    lista_cabeca.append(y)
+
+    # salvando cada posição passada
+
+    lista_tudo.append(lista_cabeca)
+
+    # delimitando o tamanho da cobra
+
+    if len(lista_tudo) > contador_de_pontos+1:
+        lista_tudo.pop(0)
+
+    # chamando a funcao
+
+    movecorpo(lista_tudo)
+    
     pygame.display.update()  # Atualiza a tela
 
 pygame.mixer.music.stop()
