1079ee07935e21087eb53be9e8ccbb0579470254
Nyh0n <fp354575@gmail.com>
2025-04-23 10:22:13 -0300

    Controlar jogador
    ++ Adição ++
    
    +Função para controla o jogador

--- versão instável/Funcoes.py
+++ versão instável/Funcoes.py
@@ -1,3 +1,6 @@
+import pygame
+pygame.init()
+
 def Constates():
     rodando = True
     largura = 640
@@ -12,8 +15,39 @@ def Cores():
     VERDE = (0, 255, 0)
     return PRETO,BRANCO,AZUL,VERMELHO,VERDE
 
-def Variaveis():
-    velocidade = 7
-    x = 0
-    y = 0
-    return velocidade,x,y
+def Variaveis(largura,altura):
+    velocidade = 10
+    tamanho_bloco = 30
+    x = (largura - tamanho_bloco) // 2
+    y = (altura - tamanho_bloco) // 2
+    x_controle = 0
+    y_controle = 0
+    FPS = 60
+    return velocidade,tamanho_bloco,x,y,x_controle,y_controle,FPS
+
+def teclados(x_controle, y_controle, vel,x,y):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return None
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP and y_controle != vel:
+                y_controle = -vel
+                x_controle = 0
+
+            elif event.key == pygame.K_DOWN and y_controle != -vel:
+                y_controle = vel
+                x_controle = 0
+
+            elif event.key == pygame.K_LEFT and x_controle != vel:
+                x_controle = -vel
+                y_controle = 0
+
+            elif event.key == pygame.K_RIGHT and x_controle != -vel:
+                x_controle = vel
+                y_controle = 0
+
+    x += x_controle
+    y += y_controle
+
+    return x_controle, y_controle,x,y
--- versão instável/VersãoInstavel.py
+++ versão instável/VersãoInstavel.py
@@ -5,18 +5,28 @@ rodando, largura, altura = Funcoes.Constates() # Constantes
 
 PRETO,BRANCO,AZUL,VERMELHO,VERDE = Funcoes.Cores() # Cores
 
-Vel,x,y = Funcoes.Variaveis() # Outras Variaveis
+Vel,tamanho_bloco,x,y,x_controle,y_controle,FPS = Funcoes.Variaveis(largura,altura) # Outras Variaveis
 
 pygame.init()
 
 tela = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption("Jogo Da Cobrinha")
 
+clock = pygame.time.Clock()  # Cria o relógio
+
 while rodando:
+    clock.tick(FPS)
     tela.fill(PRETO)
+    
+    # Teclado
+    teclado = Funcoes.teclados(x_controle, y_controle,Vel,x,y)
+    if teclado is None:
+        rodando = False
+    else:
+        x_controle, y_controle,x,y = teclado
+    # Teclado
 
-
-    jogador = pygame.draw.rect(tela,VERMELHO,(x,y,30,30))
+    jogador = pygame.draw.rect(tela,VERMELHO,(x,y,tamanho_bloco,tamanho_bloco))
 
     pygame.display.update()
 pygame.quit()
